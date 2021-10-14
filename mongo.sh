4ed5099d53f27771cb093732d06bd7588fcda275
Lucas Gomes <lucas.gomesgs0@gmail.com>
2021-10-13 22:30:25 -0300

    Changed docker-compose version and removed default tests

--- mongo.sh
+++ mongo.sh
@@ -2,13 +2,15 @@
 set -e;
 
 if [ -n "${DB_USER:-}" ] && [ -n "${DB_PASS:-}" ]; then
+  echo "=============== MONGO INIT SCRIPT STARTED ==============="
 	"${mongo[@]}" "$DB_NAME" <<-EOJS
 		db.createUser({
 			user: $(_js_escape "$DB_USER"),
 			pwd: $(_js_escape "$DB_PASS"),
-			roles: [ { role: 'readWrite' , db: $(_js_escape "$DB_NAME") } ]
+			roles: [ { role: "readWrite" , db: $(_js_escape "$DB_NAME") } ]
 			})
 	EOJS
+  echo "=============== MONGO INIT SCRIPT ENDED ==============="
 else
   echo "=============== MONGO INIT SCRIPT FAILED ==============="
   echo "${DB_NAME} ${DB_USER} ${DB_PASS}"
